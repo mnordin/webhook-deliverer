03bbe60b5c048cbcf96def1c4c28c5ff570a86ae
Markus Nordin <markus.nordin@gmail.com>
2024-10-28 22:05:27 +0100

    Improve https regex
    My bad, forgot to validate the end of the string. For the purpose of
    this app, I don’t really care about the url being a real one, just that
    it’s a https

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,5 +1,5 @@
 class Webhook < ApplicationRecord
   has_many :webhook_secrets
 
-  validates :url, presence: true, format: { with: /\Ahttps:\/\//, message: "must be https protocol" }
+  validates :url, presence: true, format: { with: /\Ahttps:\/\/.*\z/, message: "must be https protocol" }
 end
