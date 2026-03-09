23027ad68bf75745cdddb6321b002d0e5e58f65e
Markus Nordin <markus.nordin@gmail.com>
2026-03-09 20:43:37 +0100

    Show webhook delivery page
    This simple page just lists some attributes and a table for each
    delivery attempt.

--- app/controllers/webhook_deliveries_controller.rb
+++ app/controllers/webhook_deliveries_controller.rb
@@ -1,5 +1,9 @@
 class WebhookDeliveriesController < ApplicationController
   def index
-    @webhook_deliveries = WebhookDelivery.order(id: :desc)
+    @webhook_deliveries = WebhookDelivery.includes(:webhook_delivery_attempts).order(id: :desc)
+  end
+
+  def show
+    @webhook_delivery = WebhookDelivery.includes(:webhook_delivery_attempts).find(params[:id])
   end
 end
