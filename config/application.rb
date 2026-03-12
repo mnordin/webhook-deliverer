94f4b9ca2f8b662b29f9e78ad88a810f6e222c7f
Markus Nordin <markus.nordin@gmail.com>
2026-03-12 21:49:04 +0100

    Remove api_only config setting
    When I started with this app, the app was mainly focused on the model
    structure, but not anymore

--- config/application.rb
+++ config/application.rb
@@ -24,11 +24,6 @@ module WebhookDeliverer
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-
     config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
     config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
     config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
